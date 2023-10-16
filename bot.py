fbd0a4add74c39885b7ab19fd563b5a6b688dff2
goku99875 <147248319+goku99875@users.noreply.github.com>
2023-10-16 22:36:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,1 @@
 import telebot
-from telebot import custom_filters
-import time
-
-TOKEN = "6657389725:AAGEp83C3rDXTZZ0d7dIczOFB-3rvwt0t-0"
-MSG = "Join Binverse for more giveaways"
-bot = telebot.TeleBot(TOKEN)
-
-
-@bot.message_handler(commands=['start'])
-def not_admin(message):
-    bot.send_message(message.chat.id, "Your Bot is Running Fine")
-    while(True):
-        bot.send_message(-1001617751848, MSG, time.sleep(60))
-        bot.send_message(-1001780812092, MSG, time.sleep(60))
-
-# Do not forget to register
-bot.add_custom_filter(custom_filters.ChatFilter())
-bot.polling(none_stop=True)
